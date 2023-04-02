704f1cc77d0e9d602657a6f4ba159c05dba7b6e5
Someda.K <keiichiro.someda@agaroot.co.jp>
2023-04-03 02:41:00 +0900

    12. ブログテーブルの用意

---
+++ src/database/factories/BlogFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class BlogFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->realText(20),
+            'body' => $this->faker->realText(100),
+        ];
+    }
+}
