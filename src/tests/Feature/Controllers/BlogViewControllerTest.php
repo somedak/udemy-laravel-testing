563943dc758d941d3e85f5244395a037192f76a4
Someda.K <keiichiro.someda@agaroot.co.jp>
2023-04-03 03:10:24 +0900

    13. ブログのタイトルの一覧表示

--- src/tests/Feature/Controllers/BlogViewControllerTest.php
+++ src/tests/Feature/Controllers/BlogViewControllerTest.php
@@ -2,17 +2,30 @@
 
 namespace Tests\Feature\Controllers;
 
+use App\Models\Blog;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class BlogViewControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
     /** @test index */
     function ブログのTOPページを開ける()
     {
+        $blog1 = Blog::factory()->create();
+        $blog2 = Blog::factory()->create();
+        $blog3 = Blog::factory()->create();
+        Blog::factory()->create(['title' => 'かきくけこ']);
+
         $response = $this->get('/');
 
-        $response->assertOk();
+        $response->assertViewIs('index')
+            ->assertOk()
+            ->assertSee($blog1->title)
+            ->assertSee($blog2->title)
+            ->assertSee($blog3->title)
+            ->assertSee('かきくけこ');
     }
 }
