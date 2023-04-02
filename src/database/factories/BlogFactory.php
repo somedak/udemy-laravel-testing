118718a2f688eb16220a6d4142ff093199e03cdc
Someda.K <keiichiro.someda@agaroot.co.jp>
2023-04-03 08:34:39 +0900

    17. ブログの一覧で公開したものだけを表示

--- src/database/factories/BlogFactory.php
+++ src/database/factories/BlogFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Blog;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -16,8 +17,18 @@ class BlogFactory extends Factory
     {
         return [
             'user_id' => User::factory(),
+            'status' => Blog::OPEN,
             'title' => $this->faker->realText(20),
             'body' => $this->faker->realText(100),
         ];
+    }
+
+    public function seeding()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'status' => $this->faker->biasedNumberBetween(0, 1, ['\Faker\Provider\Biased', 'linearHigh']),
+            ];
+        });
     }
 }
