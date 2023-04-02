732a56ee2b38b59ee61383fc748c36288005ad0b
Someda.K <keiichiro.someda@agaroot.co.jp>
2023-04-03 03:45:45 +0900

    14. ブログの書き手も一覧表示

--- src/database/factories/BlogFactory.php
+++ src/database/factories/BlogFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class BlogFactory extends Factory
@@ -14,6 +15,7 @@ class BlogFactory extends Factory
     public function definition()
     {
         return [
+            'user_id' => User::factory(),
             'title' => $this->faker->realText(20),
             'body' => $this->faker->realText(100),
         ];
---
+++ src/tests/Unit/Models/BlogTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+// use PHPUnit\Framework\TestCase;
+
+use App\Models\Blog;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class BlogTest extends TestCase
+{
+    use RefreshDatabase;
+    
+    /** @test user */
+    function userリレーションを返す()
+    {
+        $blog = Blog::factory()->create();
+
+        $this->assertInstanceOf(User::class, $blog->user);
+    }
+}
