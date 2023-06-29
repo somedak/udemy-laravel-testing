e0650be7ea01050c39126926188b7e5b706395fe
Someda.K <keiichiro.someda@agaroot.co.jp>
2023-06-29 11:47:29 +0900

    20. コメント、ビフォー＆アフター

--- src/resources/views/blog/show.blade.php
+++ src/resources/views/blog/show.blade.php
@@ -10,5 +10,12 @@
 <div>{!! nl2br(e($blog->body)) !!}</div>
 
 <p>書き手：{{ $blog->user->name }}</p>
-    
+
+<h2>コメント</h2>
+@foreach($blog->comments()->oldest()->get() as $comment)
+    <hr>
+    <p>{{ $comment->name }}（{{ $comment->created_at }}）</p>
+    <p>{!! nl2br(e($comment->body)) !!}</p>
+@endforeach
+
 @endsection
