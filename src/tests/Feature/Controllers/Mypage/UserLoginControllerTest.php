a067438ff8eddb46796838de2e7fa11899c79871
Someda.K <keiichiro.someda@agaroot.co.jp>
2023-06-29 23:53:22 +0900

    26. ログイン処理（入力チェック）

--- src/tests/Feature/Controllers/Mypage/UserLoginControllerTest.php
+++ src/tests/Feature/Controllers/Mypage/UserLoginControllerTest.php
@@ -17,4 +17,23 @@ class UserLoginControllerTest extends TestCase
         $this->get('mypage/login')
             ->assertOk();
     }
+
+    /** @test login */
+    function ログイン時の入力チェック()
+    {
+        $url = 'mypage/login';
+
+        $this->from($url)->post($url, [])
+            ->assertRedirect($url);
+
+        $this->post($url, ['email' => ''])
+            ->assertSessionHasErrors(['email' => 'メールアドレスは必ず指定してください。']);
+        $this->post($url, ['email' => 'aa@bb@cc'])
+            ->assertSessionHasErrors(['email' => 'メールアドレスには、有効なメールアドレスを指定してください。']);
+        $this->post($url, ['email' => 'aa@ああ.いい'])
+            ->assertSessionHasErrors(['email' => 'メールアドレスには、有効なメールアドレスを指定してください。']);
+
+        $this->post($url, ['password' => ''])
+            ->assertSessionHasErrors(['password' => 'パスワードは必ず指定してください。']);
+    }
 }
