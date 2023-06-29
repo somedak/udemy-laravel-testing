f50eaab4b712ad6a5df9297dfbc1b7b91094491b
Someda.K <keiichiro.someda@agaroot.co.jp>
2023-06-30 04:32:37 +0900

    29. 認証していれば、マイページを開ける

--- src/tests/Feature/Controllers/Mypage/BlogMypageControllerTest.php
+++ src/tests/Feature/Controllers/Mypage/BlogMypageControllerTest.php
@@ -11,11 +11,19 @@ use Tests\TestCase;
  */
 class BlogMypageControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
     /** @test index */
     function 認証している場合に限り、マイページを開ける()
     {
         // 認証していない場合
         $this->get('mypage/blogs')
             ->assertRedirect('mypage/login');
+
+        // 認証済みの場合
+        $this->login();
+
+        $this->get('mypage/blogs')
+            ->assertOk();
     }
 }
